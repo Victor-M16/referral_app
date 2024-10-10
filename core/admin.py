3bbf9192e89104cc605864613d81a9cc4c9fb714
Victor Mjimapemba <vcmjimapemba@gmail.com>
2024-10-10 08:15:05 +0200

    added UserAdmin to fix lgging in issues

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    model=User
+
 
 # Register your models here.
 admin.site.register(Hospital)
-admin.site.register(User)
 admin.site.register(Patient)
 admin.site.register(MedicalHistory)
 admin.site.register(Diagnostic)
 admin.site.register(Equipment)
 admin.site.register(Referral)
+admin.site.register(User, CustomUserAdmin)
